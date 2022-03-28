578ba2fb468a521373cf959e55d91cf98d90bbcc
Roman Dryndik <dryndikroman@gmail.com>
2022-03-28 19:01:33 +0200

    Added blogpost.py

---
+++ blogpost.py
@@ -0,0 +1,29 @@
+class BlogPostHistory:
+    FILENAME = "posts.txt"
+    SEPARATOR = ","
+
+    def __init__(self, title, desc):
+        self._title = title
+        self._desc = desc
+
+    def save(self):
+        with open(self.FILENAME, "a+") as f:
+            data = f"{self._title}{self.SEPARATOR}{self._desc}\n"
+            f.write(data)
+
+    def change_title(self, title):
+        self._title = title
+        try:
+            self.save()
+        except OSError:
+            raise Exception("BlogPostHistory can't save history")
+
+    def change_description(self, desc):
+        self._desc = desc
+        try:
+            self.save()
+        except OSError:
+            raise Exception("BlogPostHistory can't save history")
+
+    def get_properties(self):
+        return self._title, self._desc
