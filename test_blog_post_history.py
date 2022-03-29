3c8aacd8681ebb4227ebc62fe4a056f9c79a0e77
Patryk <szkolenieGIT@sda.pl>
2022-03-29 19:48:56 +0200

    Added properties to read title and desc.

---
+++ test_blog_post_history.py
@@ -0,0 +1,42 @@
+import unittest
+from unittest.mock import Mock
+from blogpost import BlogPostHistory
+
+
+class TestBlogPostHistory(unittest.TestCase):
+    def test_change_title_should_change_title(self):
+        expected_title = "New title"
+        blogpost = BlogPostHistory("Example", "...")
+        blogpost.save = Mock()
+
+        blogpost.change_title(expected_title)
+
+        self.assertEqual(blogpost.title, expected_title)
+        blogpost.save.assert_called_once_with()
+
+    def test_change_description_should_change_description(self):
+        blogpost = BlogPostHistory("Example", "...")
+        expacted_description = "Some description"
+        blogpost.save = Mock()
+
+        blogpost.change_description(expacted_description)
+
+        self.assertEqual(blogpost.description, expacted_description)
+        blogpost.save.assert_called_once_with()
+
+    def test_change_tittle_should_raise_on_os_error(self):
+        blogpost = BlogPostHistory("Example","...")
+        blogpost.save = Mock(side_effect=OSError)
+
+        self.assertRaises(
+            Exception, blogpost.change_title, "New tittle")
+
+
+
+
+
+
+
+
+
+
