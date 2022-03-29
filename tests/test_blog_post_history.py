d8299904c76c86dee04b708da5b5e83888c46c72
Paweł Przytomski <przytop@gmail.com>
2022-03-29 19:50:31 +0200

    Added properties to read title and description.

--- tests/test_blog_post_history.py
+++ tests/test_blog_post_history.py
@@ -1,6 +1,5 @@
 import unittest
 from unittest.mock import Mock
-
 from blogpost import BlogPostHistory
 
 
@@ -9,28 +8,19 @@ class TestBlogPostHistory(unittest.TestCase):
         expected_title = "New title"
         blogpost = BlogPostHistory("Example", "...")
         blogpost.save = Mock()
-
         blogpost.change_title(expected_title)
-
-        title, desc = blogpost.get_properties()
-        self.assertEqual(title, expected_title)
+        self.assertEqual(blogpost.title, expected_title)
         blogpost.save.assert_called_once_with()
 
     def test_change_description_should_change_description(self):
         blogpost = BlogPostHistory("Example", "...")
         expected_description = "Some description"
         blogpost.save = Mock()
-
         blogpost.change_description(expected_description)
-
-        title, desc = blogpost.get_properties()
-        self.assertEqual(desc, expected_description)
+        self.assertEqual(blogpost.description, expected_description)
         blogpost.save.assert_called_once_with()
 
     def test_change_title_should_raise_on_os_error(self):
         blogpost = BlogPostHistory("Example", "...")
         blogpost.save = Mock(side_effect=OSError)
-
-        self.assertRaises(
-            Exception, blogpost.change_title, "New title"
-        )
+        self.assertRaises(Exception, blogpost.change_title, "New title")
